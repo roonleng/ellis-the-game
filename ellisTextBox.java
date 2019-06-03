1748fb44eb8dbc71870ac5b63df6d4c20443e5ad
roonleng <42977073+roonleng@users.noreply.github.com>
2019-06-03 13:34:14 -0400

    Add files via upload

---
+++ ellisTextBox.java
@@ -0,0 +1,5 @@
+package ellisGame;
+
+public class ellisTextBox {
+
+}
---
+++ test.java
@@ -0,0 +1,101 @@
+package ellisGame;
+
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+
+public class test extends JLabel
+{
+	private ArrayList<String> dialogue = new ArrayList<String>();
+	public test()
+	{
+	dialogue.add("It was a beautiful day in the valley- birds were chirping, students were learning. Everything was right. Until you (Mr. Ellis see) something terrible in the newspaper...");
+	dialogue.add("Ellis: What??? That’s the last freakin’ straw!");
+	dialogue.add("And with all of your superhuman strength, you run to Michigan in under a minute. Your purpose? To conquer Michigan, and establish ELLISYLVANIA!");
+	dialogue.add("You find yourself in the middle of Michigan.");
+	dialogue.add("Ellis: Hmm… how do I conquer Michigan?");
+	dialogue.add("Amidst your contemplation, a street urchin approaches you.");
+	dialogue.add("Ellis: Hey kid, who runs this place?");
+	dialogue.add("Kid: You mean like this vast, empty, desolate, cesspool and wasteland that is the entirety of Michigan?");
+	dialogue.add("Ellis: Yes");
+	dialogue.add("Kid: King Henry Ford, the terrible tyrant of Michigan. He resides in his mansion on the northern peninsula. He anticipated your arrival, and went into hiding. You’ll need a boat to reach him.");
+	dialogue.add("Ellis: Hey, thanks kid.");
+	
+	//detroit
+	dialogue.add("You enter Detroit. Atop of a towering Marriott Hotel is a metallic spire, perfect for the Ellis Warship.");
+	dialogue.add("Just before our hero can remove the spire from the building, he hears a voice.");
+	dialogue.add("Eminem: Not so FAST! Till I collAPSE I’m spillin’ these RAPS, lay a hand on the city of GAS and I’ll kick your-");
+	dialogue.add("Ellis: Woah! There is no swearing in the freakin’ valley! Also, rock is far superior to hip-hop!");
+	dialogue.add("Enraged at the sight of The Real Slim Shady, you enters the fight!");
+	dialogue.add("Eminem: Waaaah!!!");
+	dialogue.add("Eminem stumbles backwards, to the ledge of the Marriott, and not dissimilar to the state of his career, Eminem falls off.");
+	dialogue.add("You got the Spire! It will serve as the great mast of your Ellis warship. Easily strapping it to your back, you continue on your journey.");
+	dialogue.add("Your eyes land on another one of the Michigan’s pitiful cities- Flint Michigan, population 900.");
+	dialogue.add("The once populous city has dwindled ever since their water crisis in 2014. You stride through the town, noting lonely civilians toting packages of overpriced bottled water.");
+	dialogue.add("In a moment of clarity you take note that it is not every citizen that makes Michigan the worst state in America, but the corrupt government and terrible infrastructure.");
+	dialogue.add("The thought of freeing all of these poor people from the shackles of Michigan, and sending them into the open arms of Ellisylvania… it fills you with determination.");
+	dialogue.add("You head to the lake, in hopes of finding something resembling a boat, but because the water is so tainted with chemicals, all that remains is a great metal hull, jutting into the shore. It seems perfect for the great Ellis Warship.");
+	dialogue.add("Before you can lift the boat (easily, with your huge muscles), you hear footsteps behind you.");
+	dialogue.add("???: Isn’t she lovely?");
+	dialogue.add("You turn around to find that Stevie Wonder is standing directly behind you.");
+	dialogue.add("Ellis: I need this boat to save Detroit, Steve");
+	dialogue.add("Stevie: Well Sir, you’ll have to Duke it out with me first!");
+	//fight
+	dialogue.add("Stevie: Wait, Mr. Ellis");
+	dialogue.add("You stand over the defeated Motown superstar.");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	dialogue.add("");
+	}
+}
